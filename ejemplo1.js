3a66c5f1d4aa62cde70e446bfab9fd4ff83259a6
daniel <ddrivera47@misena.edu>
2022-06-24 20:34:26 -0500

    segundo commit

--- ejemplo1.js
+++ ejemplo1.js
@@ -1,3 +1,20 @@
 class libros {
-    
+    constructor(autor,nombre,numerodepaginas ){
+        this.autor=autor
+        this.nombre=nombre
+        this.numerodepaginas=numerodepaginas
+    }
+    set nombrelibro(nombre1){
+
+    }
+    get mostrarlibro(){
+        
+
+    } 
 }
+let libro1=new libros ("daniel","cirm","3650")
+console.log(libro1)
+let libro2=new libros ("teo","fan","341")
+console.log(libro2)
+let libro3=new libros ("dan ","secs","650")
+console.log(libro3)
